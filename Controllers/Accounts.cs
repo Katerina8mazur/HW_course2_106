5f74e576e00d48e9e38152686acc07ade88450c4
Katerina8mazur <ek.mazurkevich@gmail.com>
2022-12-12 10:34:23 +0300

    Initial commit

---
+++ Controllers/Accounts.cs
@@ -0,0 +1,39 @@
+﻿using HttpServer_1.Attributes;
+using HttpServer_1.Models;
+using HttpServer_1.ORM;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer_1.Controllers
+{
+    [HttpController("accounts")]
+    internal class Accounts
+    {
+        //GetAccounts, GetAccountById и SaveAccount
+
+        private static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SteamDB;Integrated Security=True";
+        private static AccountDAO accountDAO = new AccountDAO(connectionString);
+
+        [HttpGET(@"\d+")]
+        public Account? GetAccountById(int id)
+            => accountDAO.Get(id);
+
+        [HttpGET("")]
+        public List<Account> GetAccounts()
+            => accountDAO.GetAll();
+
+        [HttpPOST("")]
+        public void SaveAccount(string login, string password)
+            => accountDAO.Insert(login, password);
+
+        //Get /accounts/ - список аккаунтов в формате json
+        //Get /accounts/{id} - информация об одном аккаунте
+        //Post /accounts/ - добавлять инф на сервер - принимает параметры через body
+
+        //надо брать все данные из бд
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,22 @@
+﻿ using HttpServer_1.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer_1.Models
+{
+    internal class Account
+    {
+        public int Id { get; set; }
+
+        [DBField("login")]
+        public string Login { get; set; }
+
+        [DBField("password")]
+        public string Password { get; set; }
+
+
+    }
+}
